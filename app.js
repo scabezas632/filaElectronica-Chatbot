f3c36cb3b52f446b2e827297a53c86091f5a0997
scabezas632 <samuel.cabezas.632@gmail.com>
2018-07-23 16:48:29 -0400

    Se implementa la funcion de consultarHorario

--- app.js
+++ app.js
@@ -187,6 +187,9 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
         case "obtener-ofertas":
             consultarOfertas(sender, responseText);
             break;
+        case "pedir-horario":
+            consultarHorario(sender, responseText, parameters);
+            break;
         default:
             // Acción no controlada, se envia mensaje por default
             sendTextMessage(sender, responseText);
@@ -870,7 +873,46 @@ function isDefined(obj) {
     return obj != null;
 }
 
+//========================================
+// HORARIO 
+//========================================
+function consultarHorario(sender, responseText, parameters) {
+    if (parameters.hasOwnProperty('comuna') && parameters['comuna'] != '') {
+        var request = require('request');
+        sendTextMessage(sender, 'Ok, dame un momento para consultar los horarios...');
+        request({
+            url: 'https://filaelectronica-backend.herokuapp.com/sucursal',
+            qs: {
+                comuna: parameters['comuna']
+            }
+        }, function(err, resp, body) {
+            if (!err && resp.statusCode == 200) {
+                let sucursal = JSON.parse(body);
+                if (sucursal.hasOwnProperty('sucursales') && sucursal.length == 1) {
+                    let reply = `${responseText}\n` +
+                        `${sucursal.sucursales[0]['horario']['semana']}\n` +
+                        `${sucursal.sucursales[0]['horario']['domingo']}`
+                }
+                if (sucursal.hasOwnProperty('sucursales') && sucursal.length > 1) {
+                    sendTextMessage(sender, `Tenemos ${sucursal.length} sucursales en ${parameters['comuna']},` +
+                        `¿Cuál es la sucursal que necesitas?`);
+                } else {
+                    sendTextMessage(sender, `Disculpa pero no tenemos sucursales en ${parameters['comuna']}`);
+                }
+            } else {
+                sendTextMessage(sender, 'Disculpa, pero en estos momentos no es posible revisar los horarios.');
+                console.error(err);
+            }
+        });
+    } else {
+        sendTextMessage(sender, responseText);
+    }
+}
 
+
+//========================================
+// OFERTAS 
+//========================================
 function consultarOfertas(sender, responseText) {
     var request = require('request');
     sendTextMessage(sender, 'Ok, dame un momento para consultar las ofertas...');
@@ -915,7 +957,6 @@ function consultarOfertas(sender, responseText) {
     });
 }
 
-
 // Comparar fechas
 function ofertaIsValidate(oferta) {
     const desde = new Date(oferta['ofertaInicio']);
@@ -927,97 +968,3 @@ function ofertaIsValidate(oferta) {
         return false;
     }
 }
-
-// Scan barcode from image
-function getBarcodeFromImage(imgOrId) {
-
-    console.log(imgOrId);
-
-    var UPC_SET = {
-        "3211": '0',
-        "2221": '1',
-        "2122": '2',
-        "1411": '3',
-        "1132": '4',
-        "1231": '5',
-        "1114": '6',
-        "1312": '7',
-        "1213": '8',
-        "3112": '9'
-    };
-
-    var doc = document,
-        img = "object" == typeof imgOrId ? imgOrId : doc.getElementById(imgOrId),
-        canvas = doc.createElement("canvas"),
-        width = img.width,
-        height = img.height,
-        ctx = canvas.getContext("2d"),
-        spoints = [1, 9, 2, 8, 3, 7, 4, 6, 5],
-        numLines = spoints.length,
-        slineStep = height / (numLines + 1),
-        round = Math.round;
-    canvas.width = width;
-    canvas.height = height;
-    ctx.drawImage(img, 0, 0);
-    while (numLines--) {
-        console.log(spoints[numLines]);
-        var pxLine = ctx.getImageData(0, slineStep * spoints[numLines], width, 2).data,
-            sum = [],
-            min = 0,
-            max = 0;
-        for (var row = 0; row < 2; row++) {
-            for (var col = 0; col < width; col++) {
-                var i = ((row * width) + col) * 4,
-                    g = ((pxLine[i] * 3) + (pxLine[i + 1] * 4) + (pxLine[i + 2] * 2)) / 9,
-                    s = sum[col];
-                pxLine[i] = pxLine[i + 1] = pxLine[i + 2] = g;
-                sum[col] = g + (undefined == s ? 0 : s);
-            }
-        }
-        for (var i = 0; i < width; i++) {
-            var s = sum[i] = sum[i] / 2;
-            if (s < min) { min = s; }
-            if (s > max) { max = s; }
-        }
-        var pivot = min + ((max - min) / 2),
-            bmp = [];
-        for (var col = 0; col < width; col++) {
-            var matches = 0;
-            for (var row = 0; row < 2; row++) {
-                if (pxLine[((row * width) + col) * 4] > pivot) { matches++; }
-            }
-            bmp.push(matches > 1);
-        }
-        var curr = bmp[0],
-            count = 1,
-            lines = [];
-        for (var col = 0; col < width; col++) {
-            if (bmp[col] == curr) { count++; } else {
-                lines.push(count);
-                count = 1;
-                curr = bmp[col];
-            }
-        }
-        var code = '',
-            bar = ~~((lines[1] + lines[2] + lines[3]) / 3),
-            u = UPC_SET;
-        for (var i = 1, l = lines.length; i < l; i++) {
-            if (code.length < 6) { var group = lines.slice(i * 4, (i * 4) + 4); } else { var group = lines.slice((i * 4) + 5, (i * 4) + 9); }
-            var digits = [
-                round(group[0] / bar),
-                round(group[1] / bar),
-                round(group[2] / bar),
-                round(group[3] / bar)
-            ];
-            code += u[digits.join('')] || u[digits.reverse().join('')] || 'X';
-            if (12 == code.length) { return code; break; }
-        }
-        if (-1 == code.indexOf('X')) { return code || false; }
-    }
-    return false;
-};
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
