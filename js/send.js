f89dda2d4615d673169b52e8a1b9b8ecbf7de5a7
scabezas632 <samuel.cabezas.632@gmail.com>
2018-07-23 22:03:39 -0400

    Fix funcion isDefined

--- js/send.js
+++ js/send.js
@@ -361,6 +361,18 @@ function callSendAPI(messageData) {
     });
 }
 
+function isDefined(obj) {
+    if (typeof obj == 'undefined') {
+        return false;
+    }
+
+    if (!obj) {
+        return false;
+    }
+
+    return obj != null;
+}
+
 module.exports = {
     sendToApiAi,
     sendTextMessage,
