a78e41ec8b52e81f59608441ee8c02cdd5a04329
scabezas632 <samuel.cabezas.632@gmail.com>
2018-07-24 16:53:59 -0400

    Cambio de request por axios

--- js/ubicacion.js
+++ js/ubicacion.js
@@ -8,32 +8,55 @@ function obtenerComuna(sender, location) {
     const lat = location.lat;
     const long = location.long;
     let comuna;
-    request({
-        method: 'GET',
-        url: 'https://maps.googleapis.com/maps/api/geocode/json',
-        qs: {
-            latlng: lat + ', ' + long,
-            key: config.GMAPS_API_TOKEN
-        }
-    }, function(err, response, body) {
-        if (err) {
+    axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+                latlng: lat + ', ' + long,
+                key: config.GMAPS_API_TOKEN
+            }
+        })
+        .then(resp => {
+            let location = resp.data.results[0];
+            let contador = 0;
+            while (true) {
+                if (location['address_components'][contador]['types'][0] === 'administrative_area_level_3') {
+                    comuna = location['address_components'][contador]['long_name'];
+                    break;
+                }
+                contador++;
+            }
+            console.log("COMUNA1:", comuna);
+            return comuna;
+        })
+        .catch(err => {
             send.sendTextMessage(sender, 'Disculpa, pero en estos momentos no es posible revisar los horarios.');
             console.error(err);
-        }
-        let comunaJSON = JSON.parse(body);
-        let contador = 0;
-        while (true) {
-            if (comunaJSON['results'][0]['address_components'][contador]['types'][0] === 'administrative_area_level_3') {
-                comuna = comunaJSON['results'][0]['address_components'][contador]['long_name'];
-                break;
-            }
-            contador++;
-        }
-        console.log("COMUNA1:", comuna);
-        return comuna;
-    });
-    console.log("COMUNA2:", comuna)
-    return comuna;
+        });
+    // request({
+    //     method: 'GET',
+    //     url: 'https://maps.googleapis.com/maps/api/geocode/json',
+    //     qs: {
+    //         latlng: lat + ', ' + long,
+    //         key: config.GMAPS_API_TOKEN
+    //     }
+    // }, function(err, response, body) {
+    //     if (err) {
+    //         send.sendTextMessage(sender, 'Disculpa, pero en estos momentos no es posible revisar los horarios.');
+    //         console.error(err);
+    //     }
+    //     let comunaJSON = JSON.parse(body);
+    //     let contador = 0;
+    //     while (true) {
+    //         if (comunaJSON['results'][0]['address_components'][contador]['types'][0] === 'administrative_area_level_3') {
+    //             comuna = comunaJSON['results'][0]['address_components'][contador]['long_name'];
+    //             break;
+    //         }
+    //         contador++;
+    //     }
+    //     console.log("COMUNA1:", comuna);
+    //     return comuna;
+    // });
+    // console.log("COMUNA2:", comuna)
+    // return comuna;
 }
 
 module.exports = {
