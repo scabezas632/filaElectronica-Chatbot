80459ec54c97a4a11f770917c9700f32a1175813
scabezas632 <samuel.cabezas.632@gmail.com>
2018-07-24 12:05:37 -0400

    Arreglo de exportaciones

--- js/send.js
+++ js/send.js
@@ -3,322 +3,306 @@
 const request = require('request');
 const config = require('../config/config');
 
-module.exports = {
-    sendTextMessage: function(recipientId, text) {
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                text: text
+
+function sendTextMessage(recipientId, text) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: text
+        }
+    }
+    callSendAPI(messageData);
+}
+
+/*
+ * Envia una imagen utilizando el Send API.
+ *
+ */
+function sendImageMessage(recipientId, imageUrl) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "image",
+                payload: {
+                    url: imageUrl
+                }
             }
         }
-        callSendAPI(messageData);
-    },
+    };
 
-    /*
-     * Envia una imagen utilizando el Send API.
-     *
-     */
-    sendImageMessage: function(recipientId, imageUrl) {
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                attachment: {
-                    type: "image",
-                    payload: {
-                        url: imageUrl
-                    }
-                }
-            }
-        };
-
-        callSendAPI(messageData);
-    },
-
-    /*
-     * Envia un GIF utilizando el Send API.
-     *
-     */
-    sendGifMessage: function(recipientId) {
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                attachment: {
-                    type: "image",
-                    payload: {
-                        url: config.SERVER_URL + "/assets/instagram_logo.gif"
-                    }
-                }
-            }
-        };
-
-        callSendAPI(messageData);
-    },
-
-    /*
-     * Envia un audio utilizando el Send API.
-     *
-     */
-    sendAudioMessage: function(recipientId) {
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                attachment: {
-                    type: "audio",
-                    payload: {
-                        url: config.SERVER_URL + "/assets/sample.mp3"
-                    }
-                }
-            }
-        };
-
-        callSendAPI(messageData);
-    },
-
-    /*
-     * Envia un video utilizando un Send API.
-     * example videoName: "/assets/allofus480.mov"
-     */
-    sendVideoMessage: function(recipientId, videoName) {
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                attachment: {
-                    type: "video",
-                    payload: {
-                        url: config.SERVER_URL + videoName
-                    }
-                }
-            }
-        };
-
-        callSendAPI(messageData);
-    },
-
-    /*
-     * Envia un video utilizando el Send API.
-     * example fileName: fileName"/assets/test.txt"
-     */
-    sendFileMessage: function(recipientId, fileName) {
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                attachment: {
-                    type: "file",
-                    payload: {
-                        url: config.SERVER_URL + fileName
-                    }
-                }
-            }
-        };
-
-        callSendAPI(messageData);
-    },
-
-
-
-    /*
-     * Envia un Button Message utilizando el Send API.
-     *
-     */
-    sendButtonMessage: function(recipientId, text, buttons) {
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                attachment: {
-                    type: "template",
-                    payload: {
-                        template_type: "button",
-                        text: text,
-                        buttons: buttons
-                    }
-                }
-            }
-        };
-
-        callSendAPI(messageData);
-    },
-
-
-    sendGenericMessage: function(recipientId, elements) {
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                attachment: {
-                    type: "template",
-                    payload: {
-                        template_type: "generic",
-                        elements: elements
-                    }
-                }
-            }
-        };
-
-        callSendAPI(messageData);
-    },
-
-
-    sendReceiptMessage: function(recipientId, recipient_name, currency, payment_method,
-        timestamp, elements, address, summary, adjustments) {
-        // Generate a random receipt ID as the API requires a unique ID
-        var receiptId = "order" + Math.floor(Math.random() * 1000);
-
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                attachment: {
-                    type: "template",
-                    payload: {
-                        template_type: "receipt",
-                        recipient_name: recipient_name,
-                        order_number: receiptId,
-                        currency: currency,
-                        payment_method: payment_method,
-                        timestamp: timestamp,
-                        elements: elements,
-                        address: address,
-                        summary: summary,
-                        adjustments: adjustments
-                    }
-                }
-            }
-        };
-
-        callSendAPI(messageData);
-    },
-
-    /*
-     * Envia un mensaje con Quick Reply buttons.
-     *
-     */
-    sendQuickReply: function(recipientId, text, replies, metadata) {
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                text: text,
-                metadata: isDefined(metadata) ? metadata : '',
-                quick_replies: replies
-            }
-        };
-
-        callSendAPI(messageData);
-    },
-
-    /*
-     * Se le envia al usuario el "Visto"
-     *
-     */
-    sendReadReceipt: function(recipientId) {
-
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            sender_action: "mark_seen"
-        };
-
-        callSendAPI(messageData);
-    },
-
-    /*
-     * Se prende indicador de "Esta escribiendo..."
-     *
-     */
-    sendTypingOn: function(recipientId) {
-
-
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            sender_action: "typing_on"
-        };
-
-        callSendAPI(messageData);
-    },
-
-    /*
-     * Se apaga indicador de "Esta escribiendo..."
-     *
-     */
-    sendTypingOff: function(recipientId) {
-
-
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            sender_action: "typing_off"
-        };
-
-        callSendAPI(messageData);
-    },
-
-    // TRADUCIR
-
-    /*
-     * Send a message with the account linking call-to-action
-     *
-     */
-    sendAccountLinking: function(recipientId) {
-        var messageData = {
-            recipient: {
-                id: recipientId
-            },
-            message: {
-                attachment: {
-                    type: "template",
-                    payload: {
-                        template_type: "button",
-                        text: "Welcome. Link your account.",
-                        buttons: [{
-                            type: "account_link",
-                            url: config.SERVER_URL + "/authorize"
-                        }]
-                    }
-                }
-            }
-        };
-
-        callSendAPI(messageData);
-    },
-
-    // module.exports = {
-    //     sendTextMessage,
-    //     sendImageMessage,
-    //     sendGifMessage,
-    //     sendAudioMessage,
-    //     sendVideoMessage,
-    //     sendFileMessage,
-    //     sendButtonMessage,
-    //     sendGenericMessage,
-    //     sendReceiptMessage,
-    //     sendQuickReply,
-    //     sendReadReceipt,
-    //     sendTypingOn,
-    //     sendTypingOff,
-    //     sendAccountLinking,
-    //     callSendAPI
+    callSendAPI(messageData);
 }
+
+/*
+ * Envia un GIF utilizando el Send API.
+ *
+ */
+function sendGifMessage(recipientId) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "image",
+                payload: {
+                    url: config.SERVER_URL + "/assets/instagram_logo.gif"
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+/*
+ * Envia un audio utilizando el Send API.
+ *
+ */
+function sendAudioMessage(recipientId) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "audio",
+                payload: {
+                    url: config.SERVER_URL + "/assets/sample.mp3"
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+/*
+ * Envia un video utilizando un Send API.
+ * example videoName: "/assets/allofus480.mov"
+ */
+function sendVideoMessage(recipientId, videoName) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "video",
+                payload: {
+                    url: config.SERVER_URL + videoName
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+/*
+ * Envia un video utilizando el Send API.
+ * example fileName: fileName"/assets/test.txt"
+ */
+function sendFileMessage(recipientId, fileName) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "file",
+                payload: {
+                    url: config.SERVER_URL + fileName
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+
+
+/*
+ * Envia un Button Message utilizando el Send API.
+ *
+ */
+function sendButtonMessage(recipientId, text, buttons) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "button",
+                    text: text,
+                    buttons: buttons
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+
+function sendGenericMessage(recipientId, elements) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "generic",
+                    elements: elements
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+
+function sendReceiptMessage(recipientId, recipient_name, currency, payment_method,
+    timestamp, elements, address, summary, adjustments) {
+    // Generate a random receipt ID as the API requires a unique ID
+    var receiptId = "order" + Math.floor(Math.random() * 1000);
+
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "receipt",
+                    recipient_name: recipient_name,
+                    order_number: receiptId,
+                    currency: currency,
+                    payment_method: payment_method,
+                    timestamp: timestamp,
+                    elements: elements,
+                    address: address,
+                    summary: summary,
+                    adjustments: adjustments
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+/*
+ * Envia un mensaje con Quick Reply buttons.
+ *
+ */
+function sendQuickReply(recipientId, text, replies, metadata) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: text,
+            metadata: isDefined(metadata) ? metadata : '',
+            quick_replies: replies
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+/*
+ * Se le envia al usuario el "Visto"
+ *
+ */
+function sendReadReceipt(recipientId) {
+
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        sender_action: "mark_seen"
+    };
+
+    callSendAPI(messageData);
+}
+
+/*
+ * Se prende indicador de "Esta escribiendo..."
+ *
+ */
+function sendTypingOn(recipientId) {
+
+
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        sender_action: "typing_on"
+    };
+
+    callSendAPI(messageData);
+}
+
+/*
+ * Se apaga indicador de "Esta escribiendo..."
+ *
+ */
+function sendTypingOff(recipientId) {
+
+
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        sender_action: "typing_off"
+    };
+
+    callSendAPI(messageData);
+}
+
+// TRADUCIR
+
+/*
+ * Send a message with the account linking call-to-action
+ *
+ */
+function sendAccountLinking(recipientId) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "button",
+                    text: "Welcome. Link your account.",
+                    buttons: [{
+                        type: "account_link",
+                        url: config.SERVER_URL + "/authorize"
+                    }]
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+
 
 /*
  * Call the Send API. The message data goes in the body. If successful, we'll 
@@ -362,4 +346,21 @@ function isDefined(obj) {
     }
 
     return obj != null;
+}
+
+module.exports = {
+    sendTextMessage,
+    sendImageMessage,
+    sendGifMessage,
+    sendAudioMessage,
+    sendVideoMessage,
+    sendFileMessage,
+    sendButtonMessage,
+    sendGenericMessage,
+    sendReceiptMessage,
+    sendQuickReply,
+    sendReadReceipt,
+    sendTypingOn,
+    sendTypingOff,
+    sendAccountLinking,
 }
