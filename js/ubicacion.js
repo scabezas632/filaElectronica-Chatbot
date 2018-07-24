db5800e6758164d67fca2dfb8b6245a367168468
scabezas632 <samuel.cabezas.632@gmail.com>
2018-07-24 16:36:46 -0400

    console.log comuna

--- js/ubicacion.js
+++ js/ubicacion.js
@@ -7,8 +7,6 @@ const send = require('./send');
 function obtenerComuna(sender, location) {
     const lat = location.lat;
     const long = location.long;
-    let comuna;
-    const self = this;
     request({
         method: 'GET',
         url: 'https://maps.googleapis.com/maps/api/geocode/json',
@@ -22,19 +20,20 @@ function obtenerComuna(sender, location) {
             console.error(err);
         }
         let comunaJSON = JSON.parse(body);
+        let comuna;
+        const self = this;
         let contador = 0;
         while (true) {
             if (comunaJSON['results'][0]['address_components'][contador]['types'][0] === 'administrative_area_level_3') {
-                self.comuna = comunaJSON['results'][0]['address_components'][contador]['long_name'];
-                console.log("COMUNA:", self.comuna);
+                comuna = comunaJSON['results'][0]['address_components'][contador]['long_name'];
                 break;
             }
             contador++;
         }
-        console.log("COMUNA:", self.comuna);
+        console.log("COMUNA1:", comuna);
     });
-    console.log("COMUNA:", comuna)
-    return comuna;
+    console.log("COMUNA2:", self.comuna)
+    return self.comuna;
 }
 
 module.exports = {
