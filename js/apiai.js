d7799681c614667aaf8a253f29b88e2d3f96b65b
scabezas632 <samuel.cabezas.632@gmail.com>
2018-07-25 20:10:27 -0400

    Fix function isDefined

--- js/apiai.js
+++ js/apiai.js
@@ -4,7 +4,8 @@
 const apiai = require('apiai');
 const config = require('../config/config');
 const send = require('./send');
-const handle = require('./handle');
+const horario = require('./horario');
+const oferta = require('./oferta');
 
 const apiAiService = apiai(config.API_AI_CLIENT_ACCESS_TOKEN, {
     language: "en",
@@ -20,12 +21,82 @@ function sendToApiAi(sender, text, sessionIds) {
 
     apiaiRequest.on('response', (response) => {
         if (isDefined(response.result)) {
-            handle.handleApiAiResponse(sender, response);
+            handleApiAiResponse(sender, response);
         }
     });
 
     apiaiRequest.on('error', (error) => console.error(error));
     apiaiRequest.end();
+}
+
+function handleApiAiResponse(sender, response) {
+    let responseText = response.result.fulfillment.speech;
+    let responseData = response.result.fulfillment.data;
+    let messages = response.result.fulfillment.messages;
+    let action = response.result.action;
+    let contexts = response.result.contexts;
+    let parameters = response.result.parameters;
+
+    send.sendTypingOff(sender);
+
+    if (isDefined(messages) && (messages.length == 1 && messages[0].type != 0 || messages.length > 1)) {
+        let timeoutInterval = 1100;
+        let previousType;
+        let cardTypes = [];
+        let timeout = 0;
+        for (var i = 0; i < messages.length; i++) {
+
+            if (previousType == 1 && (messages[i].type != 1 || i == messages.length - 1)) {
+                timeout = (i - 1) * timeoutInterval;
+                setTimeout(handleCardMessages.bind(null, cardTypes, sender), timeout);
+                cardTypes = [];
+                timeout = i * timeoutInterval;
+                setTimeout(handleMessage.bind(null, messages[i], sender), timeout);
+            } else if (messages[i].type == 1 && i == messages.length - 1) {
+                cardTypes.push(messages[i]);
+                timeout = (i - 1) * timeoutInterval;
+                setTimeout(handleCardMessages.bind(null, cardTypes, sender), timeout);
+                cardTypes = [];
+            } else if (messages[i].type == 1) {
+                cardTypes.push(messages[i]);
+            } else {
+                timeout = i * timeoutInterval;
+                setTimeout(handleMessage.bind(null, messages[i], sender), timeout);
+            }
+
+            previousType = messages[i].type;
+
+        }
+    } else if (responseText == '' && !isDefined(action)) {
+        //API AI no puede evaluar el input
+        console.log('Pregunta desconocida: ' + response.result.resolvedQuery);
+        send.sendTextMessage(sender, "No estoy seguro de lo que me dices. ¿Puedes ser más especifico?");
+    } else if (isDefined(action)) {
+        handleApiAiAction(sender, action, responseText, contexts, parameters);
+    } else if (isDefined(responseData) && isDefined(responseData.facebook)) {
+        try {
+            console.log('Respuesta como formato de mensaje: ' + responseData.facebook);
+            send.sendTextMessage(sender, responseData.facebook);
+        } catch (err) {
+            send.sendTextMessage(sender, err.message);
+        }
+    } else if (isDefined(responseText)) {
+        send.sendTextMessage(sender, responseText);
+    }
+}
+
+function handleApiAiAction(sender, action, responseText, contexts, parameters) {
+    switch (action) {
+        case "obtener-ofertas":
+            oferta.consultarOfertas(sender, responseText);
+            break;
+        case "obtener-horario":
+            horario.consultarHorario(sender, responseText, parameters);
+            break;
+        default:
+            // Acción no controlada, se envia mensaje por default 
+            send.sendTextMessage(sender, responseText);
+    }
 }
 
 function isDefined(obj) {
