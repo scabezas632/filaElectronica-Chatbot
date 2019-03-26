106bbf8aea64e256fefecf5163ee0e5f72c6ed0d
Samuel Cabezas <samuel.cabezas@iqbuild.cl>
2019-03-25 23:40:15 -0300

    Fix ofertas

--- js/pedirOferta.js
+++ js/pedirOferta.js
@@ -22,45 +22,45 @@ const quickReplyFunctions = [{
 
 async function consultarOfertas(sender, responseText, parameters) {
     await send.sendTextMessage(sender, 'Ok, dame un momento para consultar las ofertas...');
-    request({
-        url: 'https://filaelectronica-backend.herokuapp.com/oferta'
-    }, function(err, resp, body) {
-        if (!err && resp.statusCode == 200) {
-            let oferta = JSON.parse(body);
-            if (oferta.hasOwnProperty('ofertas') && oferta.length > 0) {
-                let reply = `${responseText}\n`
-                let contador = 0;
-                for (let i = 0; i < oferta.length; i++) {
-                    // Verificar validez de la oferta
-                    if (ofertaIsValidate(oferta.ofertas[i])) {
-                        let ahorro = oferta.ofertas[i]['producto']['precio'] - oferta.ofertas[i]['precioOferta'];
-                        reply = `${reply}` +
-                            `*Producto: ${oferta.ofertas[i]['producto']['nombre']}*\n` +
-                            `Precio Normal: $${oferta.ofertas[i]['producto']['precio']}\n` +
-                            `Precio Oferta: $${oferta.ofertas[i]['precioOferta']}\n` +
-                            `Ahorro: $${ahorro}\n\n`;
+    try {
+        let resp = await axios.get(URL_API + '/oferta');
+        let reply;
 
-                        // Aumentar contador de ofertas validas
-                        contador++;
-                    }
+        let ofertas = resp.data.ofertas;
+        console.log(ofertas)
+        if (ofertas.length > 0) {
+            let reply = `${responseText}\n`
+            let contador = 0;
+            for (let i = 0; i < ofertas.length; i++) {
+                // Verificar validez de la oferta
+                if (ofertaIsValidate(ofertas[i])) {
+                    let ahorro = ofertas[i]['producto']['precio'] - ofertas[i]['precioOferta'];
+                    reply = `${reply}` +
+                        `*Producto: ${ofertas[i]['producto']['nombre']}*\n` +
+                        `Precio Normal: $${ofertas[i]['producto']['precio']}\n` +
+                        `Precio Oferta: $${ofertas[i]['precioOferta']}\n` +
+                        `Ahorro: $${ahorro}\n\n`;
+
+                    // Aumentar contador de ofertas validas
+                    contador++;
                 }
-                if (contador != 0) {
-                    send.sendTextMessage(sender, reply);
-                } else {
-                    reply = 'Disculpa pero no existen ofertas vigentes' +
-                        '¿Hay algo más en lo que pueda ayudarte?';
-                    send.sendQuickReply(sender, reply, quickReplyFunctions);
-                }
+            }
+            if (contador != 0) {
+                send.sendTextMessage(sender, reply);
             } else {
                 reply = 'Disculpa pero no existen ofertas vigentes' +
                     '¿Hay algo más en lo que pueda ayudarte?';
                 send.sendQuickReply(sender, reply, quickReplyFunctions);
             }
         } else {
-            send.sendTextMessage(sender, 'Disculpa, pero en estos momentos no es posible revisar las ofertas');
-            console.error(err);
+            reply = 'Disculpa pero no existen ofertas vigentes' +
+                '¿Hay algo más en lo que pueda ayudarte?';
+            send.sendQuickReply(sender, reply, quickReplyFunctions);
         }
-    });
+    } catch (error) {
+        send.sendTextMessage(sender, 'Disculpa, pero en estos momentos no es posible revisar las ofertas');
+        console.error(error);
+    }
 }
 
 // Comparar fechas 
